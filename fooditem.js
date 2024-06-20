c09078bcc237cfb3323e17b3422404a3943e9dcc
Kashvi Jain <91721769+Kashvi1234@users.noreply.github.com>
2024-06-20 21:39:43 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,405 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Hot coffee',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/hot coffee.jpeg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'cold coffee',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/cold coffee.jpeg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Tea',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'images/tea.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Aloo Parantha',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/aloo.jpeg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Paneer parantha',
+    category : 'biryani',
+    rating : 4.3,
+    price: 10,
+    img: 'images/paneer.jpeg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kadahi Paneer',
+    category : 'biryani',
+    rating : 4.3,
+    price: 12,
+    img: 'images/kadhai.jpeg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Paneer Lababdar',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/lababdar.jpeg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Fried Rice',
+    category : 'biryani',
+    rating : 4.3,
+    price: 20,
+    img: 'images/fried rice.jpeg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Chilli garlic noodles',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/chilli garlic noodles.jpeg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Spicy Mexican',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'images/spicy mexican.jpeg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Farm Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'images/farm pizza.jpeg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Margreita',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: 'images/margreita.jpeg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Veggie Delight Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: 'images/veggie delight.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Mashroom Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'images/mushroom.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Paneer Tikka Pizza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 20,
+    img: 'images/paneer tikka.jpeg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Hot and spicy paneer burger',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/hot and spicy paneer.jpeg',
+    quantity: 1
+    
+
+},
+{
+    id: 17,
+    name: 'Crispy veg burger',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: 'images/crispy veg.jpeg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Mexican patty burger',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/mexican patty.jpeg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Cold Coffee',
+    category : 'paneer',
+    rating : 4.3,
+    price: 12,
+    img: 'images/cold coffee.jpeg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Oreo shake',
+    category : 'paneer',
+    rating : 4.3,
+    price: 8,
+    img: 'images/oreo shake.jpeg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Hot and spicy veg burger ',
+    category : 'paneer',
+    rating : 4.3,
+    price: 7,
+    img: 'images/hot and spicy veg.jpeg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Mayo burger',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 5,
+    img: 'images/mayo.jpeg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Veg burger',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 8,
+    img: 'images/veg burger.jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Non veg burger',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/non veg burger.jpeg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Grilled sandwich',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 5,
+    img: 'images/grilled.jpeg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'White sauce pasta',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/white sauce.jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Red sauce pasta',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 10,
+    img: 'images/red sauce.jpeg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Fried Chicken',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/fried chicken.jpeg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Smoothie',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/smoothie.jpeg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/cabbage-momos-.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'images/chinese/Manchurian.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili paneer',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: 'images/chinese/Chili-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.3,
+    price: 16,
+    img: 'images/chinese/chowmin.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 14,
+    img: 'images/chinese/spring-rolls.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan paneer',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chinese/szechuan-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/veg-fried-rice.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 18,
+    img: 'images/south indian/Butter-Masala-Dosa.png',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.3,
+    price: 20,
+    img: 'images/south indian/idli-with-rice-flour.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 12,
+    img: 'images/south indian/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'south indian',
+    rating : 4.3,
+    price: 10,
+    img: 'images/south indian/mysore-bonda.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 15,
+    img: 'images/south indian/onion-uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 40,
+    img: 'images/south indian/plain-dosa.jpeg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 25,
+    img: 'images/south indian/Rava-Uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.3,
+    price: 34,
+    img: 'images/south indian/sambhar-vada.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
